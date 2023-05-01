65d082120348929fb6b1a1b0b8c0ac2783ce86ca
Michaeel4 <s1074476@stud.sbg.ac.at>
2023-05-01 20:07:08 +0200

    Finished Task 1

---
+++ src.demo/desmoj/demo/histo_time/Demo.java
@@ -0,0 +1,15 @@
+package desmoj.demo.histo_time;
+
+
+
+import desmoj.core.statistic.*;
+import desmoj.core.simulator.Model;
+
+public class Demo {
+
+
+
+    public static void main(String[] args) {
+    }
+
+};
---
+++ src.demo/desmoj/demo/histo_time/KundeEntity.java
@@ -0,0 +1,16 @@
+package desmoj.demo.histo_time;
+
+import desmoj.core.simulator.*;
+
+// zur Darstellung von Kunden
+// -> einfache Version, keine speziellen Attribute notwendig
+public class KundeEntity extends Entity {
+
+    // Konstruktor
+    // Par 1: Modellzugehoerigkeit
+    // Par 2: Name der Entitaet
+    // Par 3: show inInTrace
+    public KundeEntity(Model owner, String name, boolean showInTrace) {
+	   super(owner, name, showInTrace);
+    }
+}
